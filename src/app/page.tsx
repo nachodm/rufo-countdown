5c50595dd2169a75dd9c959a3019312e8d9f27d4
Ignacio Domingo <ignacdm10@outlook.com>
2024-09-21 17:51:31 +0200

    feat: added message and solved countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 import react from "react"
-import Image from "next/image"
 
 import Countdown from "react-countdown"
 import { Time } from "./types"
 import { Watch } from "react-loader-spinner"
 
-const Completionist = () => <span>El código secreto para liberar vuestro tardío regalo de boda se encuentra en el interior de la caja. Buena suerte.</span>
+const Completionist = () => (
+    <span id="completed">
+        Para liberar vuestro tardío regalo de boda el código secreto debereis hallar. Para resolverlo, necesitaréis ver muy detenidamente el siguiente link.
+        Insistimos: MUY detenidamente. Para ayudaros, la caja contiene una herramienta esencial para resolverlo. De hecho, la herramienta tiene a su vez también
+        la mitad del código... Buena suerte.
+    </span>
+)
 
 const renderer = (time: Time) => {
     const { days, hours, minutes, seconds, completed } = time
@@ -31,7 +36,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <div className="relative z-[-1] flex place-items-center">
-                <Countdown date={new Date("2024-09-23")} renderer={renderer}>
+                <Countdown date={new Date("2024-09-24")} renderer={renderer}>
                     <span>The end</span>
                 </Countdown>
             </div>
